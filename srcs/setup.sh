57104324a51b723cedf878438ba2cc2763561247
albgarci <albgarci@student.42madrid.com>
2023-01-27 01:17:40 +0100

    simplified config

--- srcs/setup.sh
+++ srcs/setup.sh
@@ -1,15 +1,14 @@
-###########
 
-#replace the content of this variable to the path of the folder you want to work with
-DLC_WORK_DIR="$HOME/Documents/ft_containers"
+source ../config
+envsubst < docker-compose-template > docker-compose.yml
 
-##############
-
-sed -i '' 's|replace_me|'"$DLC_WORK_DIR"'|g' docker-compose.yml
-
-if [ ! -f clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar.xz ] && [ ! -f clang.tar.xz ]
+if [ ! -f clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar.xz ] && [ ! -f clang.tar.xz ] 
 then
-	curl -kL --output clang.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.6/clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar.xz && \
+	curl -kL --output clang.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.6/clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+fi
+
+if [ ! -d clang_15 ]
+then
  	tar -xvf "clang.tar.xz" && \
 	mv clang+llvm-15.0.6-x86_64-linux-gnu-ubuntu-18.04 clang_15
 fi
